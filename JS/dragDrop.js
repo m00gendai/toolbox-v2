248f48ec1dbdd16af2da10f625c13d6451728c85
Marcel Weber <mrweber@gmx.ch>
2021-10-25 06:22:19 +0000

    Added type check to localStorage values Added Achievement

--- JS/dragDrop.js
+++ JS/dragDrop.js
@@ -2,19 +2,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
     
 
     let keys = Object.keys(localStorage)
+
+    let hasAchievement
     
     for(let i=0;i<keys.length;i++){
-        let favorites = JSON.parse(localStorage.getItem(keys[i]))  
-        if(favorites[0] == "favoritesBar"){
-            document.getElementById(favorites[0]).appendChild(document.getElementById(favorites[1]))
-            document.getElementById(favorites[1]).classList.add("favoriteLinkFixedWidth")
-        } else if(favorites[0] == "favoritesBarToggle"){
-            let favoriteStyle = favorites[1]
-            document.getElementById("favoritesBar").style.display = favoriteStyle
-            document.getElementById("favoritesTogglor").innerHTML = `<i class="${favorites[2]} fa-star"></i>`
-            document.getElementById("quickLinks").style.margin = `${favorites[3]}rem 0 2rem 0`
+		if(typeof(localStorage.getItem(keys[i])) == "string"){
+			let favorites = JSON.parse(localStorage.getItem(keys[i]))  
+			if(favorites[0] == "favoritesBar"){
+				document.getElementById(favorites[0]).appendChild(document.getElementById(favorites[1]))
+				document.getElementById(favorites[1]).classList.add("favoriteLinkFixedWidth")
+			} else if(favorites[0] == "favoritesBarToggle"){
+				let favoriteStyle = favorites[1]
+				document.getElementById("favoritesBar").style.display = favoriteStyle
+				document.getElementById("favoritesTogglor").innerHTML = `<i class="${favorites[2]} fa-star"></i>`
+				document.getElementById("quickLinks").style.margin = `${favorites[3]}rem 0 2rem 0`
+			} else if(favorites[0] == "achievement"){
+				hasAchievement = true
+			}
         }
-        
     }
         
     const drake = dragula(
@@ -30,14 +35,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
     );
 
     drake.on('drop', function(el, target, source, sibling){
-        if(localStorage.getItem("toolbox_favorite_" + el.id) && target.id=="favoritesBar"){
+        console.log(hasAchievement)
+        if(localStorage.getItem("toolbox_favorite_" + el.id) && target.id=="favoritesBar" && source.id != "favoritesBar"){
+            if(!hasAchievement){
+                setTimeout(function(){
+                    document.getElementById("achievementContainer").style.display = "flex"
+                }, 0)   
+                setTimeout(function(){
                     document.getElementById("achievementDetail").innerText = "Tried adding the same Favorite twice"
                     document.getElementById("achievement").style.width = "30vw"
-                    setTimeout(function(){
-                        document.getElementById("achievement").style.width = "0vw"
-                    }, 5000)
+                },500)
+                setTimeout(function(){
+                    document.getElementById("achievement").style.width = "0vw"
+                }, 6000)
+                setTimeout(function(){
+                    document.getElementById("achievementContainer").style.display = "none"
+                }, 6500)
+                localStorage.setItem("toolbox_achievement", JSON.stringify(["achievement", "yes"]))
+            }
             drake.cancel(true);
             el.classList.remove("favoriteLinkFixedWidth")
+        } else if(localStorage.getItem("toolbox_favorite_" + el.id) && target.id=="favoritesBar" && source.id == "favoritesBar"){
+             localStorage.removeItem("toolbox_favorite_" + el.id)
+             localStorage.setItem("toolbox_favorite_" + el.id, JSON.stringify([target.id, el.id]))
         }
         
         if(target.id == "favoritesBar" && document.getElementById(target.id).childElementCount <= 9){ // eight elements, but the hint message counts as a child element, too
