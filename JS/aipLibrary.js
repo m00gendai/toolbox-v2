814924326e5f5df080afb89d845b5ec5ee958085
Marcel Weber <mrweber@gmx.ch>
2021-10-26 10:41:20 +0000

    Implemented logic for AIP Tile sorting

--- JS/aipLibrary.js
+++ JS/aipLibrary.js
@@ -1,46 +1,95 @@
 function loadAipLibraryCode(){
     
+    
+    // C O N S T A N T S
+    
+    
+    const aipBlockTypes = [
+        {target: "spanGreen", class: "eaip", text: "External link to eAIP", clicked: false},
+        {target: "spanYellow", class: "toolbox", text: "Internal link to eAIP", clicked: false},
+        {target: "spanRed", class: "bookaip", text: "Only physical AIP", clicked: false},
+        {target: "spanBlack", class: "noaip", text: "No information at all", clicked: false},
+    ]
+    
+    const aipButtonSymbols = {symbolShow: '<i class="far fa-eye"></i>', symbolHide: '<i class="far fa-eye-slash"></i>'}
+    
+    const aipLinkGeneratorSpace = document.getElementById("aipLinks");
+    
+    
+    // Q U E R Y   A I P   T I L E S   E V E N T   H A N D L E R
+    
+    
     document.getElementById("aipBar").addEventListener("keyup", function(){
         displayAIP()
     })
     
-    // A I P   Q U E R Y   I N P U T   F U N C T I O N
-
-
-function linkAIP(icao) { // icao = passed parameter from onclick
-    if (icao in linkCollectionAIP) {
-        let linkAIP = linkCollectionAIP[icao];
-    }
-    window.open(linkAIP);
-}
-
-
- // A I P   T I L E   D I S P L A Y   F U N C T I O N
-
- 
-function displayAIP() {
-    let inputA  = document.getElementById("aipBar");
-    let inputB  = inputA.value.toLowerCase();
-    let inputC  = String(inputB);
-    let rows    = document.getElementById("aipLinks").querySelectorAll(".aipLinkBox");
-    if (inputA.value != null) {
-        for (i=0; i<rows.length; i++) {
-            let sourcez = document.getElementById("aipLinks").getElementsByTagName("p")[i].innerHTML.toLowerCase();
-            let foundz  = sourcez.indexOf(inputB) !== -1;
-            if (foundz) {
-                rows[i].style.display = "block";
+    
+    // T O G G L E   A I P   T I L E S
+       
+    
+    for(let i=0;i<aipBlockTypes.length;i++){
+        document.getElementById(aipBlockTypes[i].target).innerHTML = aipBlockTypes[i].text + aipButtonSymbols.symbolShow
+        document.getElementById(aipBlockTypes[i].target).addEventListener("click", function(e){
+            // clicked true = HIDE elements 
+            // clicked false = SHOW elements 
+            if(aipBlockTypes[i].clicked == false){
+                aipBlockTypes[i].clicked = true
+                document.getElementById(aipBlockTypes[i].target).innerHTML = aipBlockTypes[i].text + aipButtonSymbols.symbolHide
+                toggleAipTiles("none",i)
             } else {
-                rows[i].style.display = "none";
+                aipBlockTypes[i].clicked = false
+                document.getElementById(aipBlockTypes[i].target).innerHTML = aipBlockTypes[i].text + aipButtonSymbols.symbolShow
+                toggleAipTiles("flex",i)
+            }
+        })
+    }
+        
+    function toggleAipTiles(display,i){
+        let rows = aipLinkGeneratorSpace.querySelectorAll(".aipLinkBox");
+        for(let j=0;j<rows.length;j++){
+            if(rows[j].classList.contains(aipBlockTypes[i].class)){
+                rows[j].style.display = display
             }
         }
     }
-}
+              
+    
+    // A I P   Q U E R Y   I N P U T   F U N C T I O N
 
-// A I P   T I L E   G E N E R A T O R
+
+    function linkAIP(icao) { // icao = passed parameter from onclick
+        if (icao in linkCollectionAIP) {
+            let linkAIP = linkCollectionAIP[icao];
+        }
+        window.open(linkAIP);
+    }
+
+
+    // A I P   T I L E   D I S P L A Y   F U N C T I O N
 
     
-    const aipLinkGeneratorSpace = document.getElementById("aipLinks");
-      
+    function displayAIP() {
+        let inputA  = document.getElementById("aipBar");
+        let inputB  = inputA.value.toLowerCase();
+        let inputC  = String(inputB);
+        let rows    = document.getElementById("aipLinks").querySelectorAll(".aipLinkBox");
+        if (inputA.value != null) {
+            for (i=0; i<rows.length; i++) {
+                let sourcez = document.getElementById("aipLinks").getElementsByTagName("p")[i].innerHTML.toLowerCase();
+                let foundz  = sourcez.indexOf(inputB) !== -1;
+                if (foundz) {
+                    rows[i].style.display = "flex";
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        }
+    }
+
+    
+    // A I P   T I L E   G E N E R A T O R
+
+    
     for(let i=0;i<aipTileData.length;i++){
         const aipTileDiv = document.createElement("div");
         const aipTileDivP = document.createElement("p");
@@ -96,7 +145,7 @@ function displayAIP() {
     });
 
     
-// A I P   L O G I N S   B U T T O N   H A N D L E R
+    // A I P   L O G I N S   B U T T O N   H A N D L E R
 
     
     document.getElementById("aipLogins").addEventListener("click", function(){
@@ -104,7 +153,7 @@ function displayAIP() {
     });
 
     
-// M I S S I N G   C O U N T R I E S   S P O I L E R
+    // M I S S I N G   C O U N T R I E S   S P O I L E R
 
     
     document.getElementById("misCtry").addEventListener("click", function() {
@@ -114,7 +163,6 @@ function displayAIP() {
             document.getElementById("misCtryCnt").style.display = "block";
         }
     });
-
 
     
 }
